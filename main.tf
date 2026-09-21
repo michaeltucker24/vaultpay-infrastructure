58f141fca74027ee26dfab4e2dd58f74fe25bfc0
Michael Tucker <mtucker9923@gmail.com>
2026-09-21 16:00:40 +0800

    Ignore Terraform cache and state

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/20"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name    = "vaultpay-vpc"
+    Project = "VaultPay"
+  }
 }
